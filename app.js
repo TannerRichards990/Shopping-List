91a30433595805317fd986aedba1aa04501a806a
Tanner Richards <Tanner.richards990@gmail.com>
2022-08-24 10:24:06 -0700

    added function to grab and display list from supabase in js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // importing other stuff, utility functions for:
+import { createListItem, getListItems, boughtListItem, deleteAllItems } from './fetch-utils.js';
 // working with supabase:
 import { checkAuth, signOutUser } from './fetch-utils.js';
 // pure rendering (data --> DOM):
@@ -15,10 +16,35 @@ signOutLink.addEventListener('click', signOutUser);
 /* end "boiler plate auth code" */
 
 // grab needed DOM elements on page:
-
+const formList = document.querySelector('shopping-list-form');
+const deleteListButton = document.querySelector('delete');
+const listEl = document.querySelector('list');
 // local state:
-
+let listItems = [];
 
 // display functions:
+async function displayList() {
+  
+    const list = await getListItems();
+
+    listEl.textContent = '';
+    for (let item of list) {
+        const listItemEl = document.createElement('p');
+        listItemEl.classList.add('list-item');
+        listItemEl.textContent = `${item.item} ${item.quantity}`;
+
+        if (item.bought) {
+            listItemEl.classList.add('bought');
+        } else {
+            listItemEl.classList.add('not-bought');
+            listItemEl.addEventListener('click', async () => {
+                await boughtListItem(item.id);
+
+                displayList();
+            });
+        }
+        listEl.append(listItemEl);
+    }
+}
 
 // events:
